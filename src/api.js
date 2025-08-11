b947d56e24fe8945e0dafea16134f3b406793553
Richard Frickleton <richardfrickleton@M3-MacBook-Pro.local>
2025-08-11 10:03:48 +0100

    updating secret usage in build pipeleine

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const newsAppAPI = axios.create({
-  baseURL: import.meta.env?.VITE_API_URL,
+  baseURL: process.env.API_URL,
 });
 
 export const getAllArticles = async (topic, sortBy, order) => {
